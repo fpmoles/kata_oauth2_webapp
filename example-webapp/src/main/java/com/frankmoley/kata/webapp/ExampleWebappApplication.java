ff0e23ddcdb9f77d3997cc318e7dc5699ed82c36
Frank P Moley III <frank.moley@datastax.com>
2018-08-12 18:44:46 -0500

    Initial project structure

---
+++ example-webapp/src/main/java/com/frankmoley/kata/webapp/ExampleWebappApplication.java
@@ -0,0 +1,12 @@
+package com.frankmoley.kata.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleWebappApplication.class, args);
+    }
+}
